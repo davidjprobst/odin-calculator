582c9deb20da4c5c2b4084deb057c39548d34fb8
davidjprobst <probst.dj2@gmail.com>
2025-10-06 07:31:32 -0400

    Operate function working

---
+++ script.js
@@ -0,0 +1,32 @@
+let a = 0;
+let b = 0;
+let operator = "";
+
+const add = function(a, b) {
+    return a + b;
+}
+
+const subtract = function(a, b) {
+    return a - b;
+}
+
+const multiply = function(a, b) {
+    return a * b;
+}
+
+const divide = function(a, b) {
+    return a / b;
+}
+
+const operate = function(a, b, operator) {
+    console.log(operator);
+    if (operator === "+") {
+        return add(a, b);
+    } else if (operator === "-") {
+        return subtract(a, b);
+    } else if (operator === "*") {
+        return multiply(a, b);
+    } else if (operator === "/") {
+        return divide(a, b);
+    } 
+}
